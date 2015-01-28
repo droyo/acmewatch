4ff39fabd52d31d72b4125b0a7514983eb3a4133
David Arroyo <droyo@aqwari.us>
2015-01-27 23:37:49 -0500

    Port of rsc's Watch command to the cross-platform fsnotify package

---
+++ main.go
@@ -0,0 +1,152 @@
+// Port of rsc's Watch command to use fsnotify
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"sync"
+
+	"9fans.net/go/acme"
+	"golang.org/x/exp/fsnotify"
+)
+
+var args []string
+var win *acme.Win
+var needrun = make(chan bool, 1)
+
+func usage() {
+	prog := filepath.Base(os.Args[0])
+	fmt.Fprintf(os.Stderr, "Usage: %s cmd\n", prog)
+	os.Exit(2)
+}
+
+func main() {
+	flag.Parse()
+	args = flag.Args()
+
+	if len(args) < 1 {
+		usage()
+	}
+
+	var err error
+	win, err = acme.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+	pwd, _ := os.Getwd()
+	win.Name(pwd + "/+watch")
+	win.Ctl("clean")
+	win.Fprintf("tag", "Get ")
+	needrun <- true
+	go events()
+	go runner()
+
+	watcher, err := fsnotify.NewWatcher()
+	if err := watcher.Watch("."); err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		select {
+		case event := <-watcher.Event:
+			fmt.Printf("Caught event %s\n", event)
+			if event.IsModify() || event.IsCreate() || event.IsDelete() || event.IsRename() {
+				select {
+				case needrun <- true:
+				default:
+				}
+			}
+		case err := <-watcher.Error:
+			log.Fatal(err)
+		}
+	}
+}
+
+func events() {
+	for e := range win.EventChan() {
+		switch e.C2 {
+		case 'x', 'X': // execute
+			if string(e.Text) == "Get" {
+				select {
+				case needrun <- true:
+				default:
+				}
+				continue
+			}
+			if string(e.Text) == "Del" {
+				win.Ctl("delete")
+			}
+		}
+		win.WriteEvent(e)
+	}
+	os.Exit(0)
+}
+
+var run struct {
+	sync.Mutex
+	id int
+}
+
+func runner() {
+	var lastcmd *exec.Cmd
+	for _ = range needrun {
+		run.Lock()
+		run.id++
+		id := run.id
+		run.Unlock()
+		if lastcmd != nil {
+			lastcmd.Process.Kill()
+		}
+		lastcmd = nil
+		cmd := exec.Command(args[0], args[1:]...)
+		r, w, err := os.Pipe()
+		if err != nil {
+			log.Fatal(err)
+		}
+		win.Addr(",")
+		win.Write("data", nil)
+		win.Ctl("clean")
+		win.Fprintf("body", "$ %s\n", strings.Join(args, " "))
+		cmd.Stdout = w
+		cmd.Stderr = w
+		if err := cmd.Start(); err != nil {
+			r.Close()
+			w.Close()
+			win.Fprintf("body", "%s: %s\n", strings.Join(args, " "), err)
+			continue
+		}
+		lastcmd = cmd
+		w.Close()
+		go func() {
+			buf := make([]byte, 4096)
+			for {
+				n, err := r.Read(buf)
+				if err != nil {
+					break
+				}
+				run.Lock()
+				if id == run.id {
+					win.Write("body", buf[:n])
+				}
+				run.Unlock()
+			}
+			if err := cmd.Wait(); err != nil {
+				run.Lock()
+				if id == run.id {
+					win.Fprintf("body", "%s: %s\n", strings.Join(args, " "), err)
+				}
+				run.Unlock()
+			}
+			win.Fprintf("body", "$\n")
+			win.Fprintf("addr", "#0")
+			win.Ctl("dot=addr")
+			win.Ctl("show")
+			win.Ctl("clean")
+		}()
+	}
+}
